55e1535b20103789fe1f14233aac5a6fd418fc04
ronaldwabukenda <ronald.wabukenda@salesloft.com>
2019-05-18 10:56:27 -0400

    Created server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let app = express();
+PORT = process.env.PORT || 3030;
+
+app.get('/', (req, res) => {
+
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`listening on ${PORT}`);
+
+})
