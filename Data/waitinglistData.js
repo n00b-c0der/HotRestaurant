5345884325ccd46fb08828e58753edc63394dd79
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-05-18 11:53:32 -0400

    waiting list data complete

--- Data/waitinglistData.js
+++ Data/waitinglistData.js
@@ -3,7 +3,9 @@ var waitingArray = [
       customerName: "Saima",
       customerEmail: "saima@example.com",
       phoneNumber: "000-000-0000",
-      customerID: "saimacool"
+      customerID: "saimacool",
+      customerDate: "02/02/2002",
+      customerGuests: "3"
     }
   ];
   
